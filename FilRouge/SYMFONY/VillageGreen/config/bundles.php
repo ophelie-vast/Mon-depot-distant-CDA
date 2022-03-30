18705900751a22f67c1a874f60d0762b09507ca6
Ophelie <ophelie.vast@outlook.fr>
2022-03-30 09:30:19 +0200

    CDA

--- FilRouge/SYMFONY/VillageGreen/config/bundles.php
+++ FilRouge/SYMFONY/VillageGreen/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
--- FilRouge/SYMFONY/VillageGreen/src/Controller/TousLesProduitsController.php
+++ FilRouge/SYMFONY/VillageGreen/src/Controller/TousLesProduitsController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\ProduitRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +14,14 @@ class TousLesProduitsController extends AbstractController
     /**
      * @Route("/catalogue", name="tous_les_produits")
      */
-    public function tous_les_produits(ProduitRepository $repo): Response
+    public function tous_les_produits(ProduitRepository $repo, PaginatorInterface $paginator, Request $request): Response
     {
         $liste_tous_les_produits = $repo->findAll();
+        $liste_tous_les_produits = $paginator->paginate(
+            $liste_tous_les_produits,
+            $request->query->getInt('page', 1),
+            5
+        );
 
         return $this->render('catalogue/index.html.twig', [
             'tous_produits' => $liste_tous_les_produits,
